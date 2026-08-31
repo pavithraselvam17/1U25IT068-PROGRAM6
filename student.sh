508d6451438b763ded921653aa9199e7f4693e5b
pavithraselvam17 <pavithraselvam53@gmail.com>
2026-08-31 12:10:28 +0530

    UNIX PROGRAM6 1U25IT068

--- student.sh
+++ student.sh
@@ -1,1 +1,19 @@
+Creating content with New
 
+file # cat newfile.txt
+
+Display the content
+
+#cat filename.txt
+
+Concatenate Multiple Files
+
+# cat filel.txt file2.txt
+
+Appended the text into text file:
+
+#cat file1.txt >> existingfile.txt
+
+Numbering Lines
+
+# cat -n filename.txt
