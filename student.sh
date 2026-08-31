a401c987f8e11487480d9a1871a4bcb416f20e1d
pavithraselvam17 <pavithraselvam53@gmail.com>
2026-08-31 12:35:19 +0530

    PROGRAM 6 1U25IT068

--- student.sh
+++ student.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 # Creating content with New file
-echo "This is a new file" > newfile.txt
-echo "This is also a text file" >> newfile.txt
+echo "this is a new file" > newfile.txt
+echo "this is also a text file" >> newfile.txt
 
 # Display the content
 cat newfile.txt
 
-# Create another file
-echo "This is a file two" > file2.txt
+# Create file1
+echo "this is a file one" > file1.txt
+echo "this is a text file" >> file1.txt
+
+# Create file2
+echo "this is file two" > file2.txt
+echo "this is text file" >> file2.txt
 
 # Concatenate Multiple Files
-cat newfile.txt file2.txt
+cat file1.txt file2.txt
 
-# Append the text into existing file
-cat file2.txt >> newfile.txt
+# Append file1 content into existingfile.txt
+cat file1.txt > existingfile.txt
 
-# Display the appended file
-cat newfile.txt
+# Display existing file
+cat existingfile.txt
 
 # Numbering Lines
 cat -n newfile.txt
