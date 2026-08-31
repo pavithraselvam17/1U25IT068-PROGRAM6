58c08d2dfd16d5a082eadd15d12b289726d5403b
pavithraselvam17 <pavithraselvam53@gmail.com>
2026-08-31 12:38:44 +0530

    PROGRAM6 1U25IT068

--- student.sh
+++ student.sh
@@ -1,28 +1,29 @@
 #!/bin/bash
 
-# Creating content with New file
-echo "this is a new file" > newfile.txt
-echo "this is also a text file" >> newfile.txt
+# Create newfile.txt
+echo "Hello Linux" > newfile.txt
+echo "This is a cat command practical." >> newfile.txt
 
-# Display the content
+# Display newfile.txt
 cat newfile.txt
 
-# Create file1
-echo "this is a file one" > file1.txt
-echo "this is a text file" >> file1.txt
+# Create file1.txt
+echo "First file" > file1.txt
 
-# Create file2
-echo "this is file two" > file2.txt
-echo "this is text file" >> file2.txt
+# Create file2.txt
+echo "Second file" > file2.txt
 
-# Concatenate Multiple Files
+# Concatenate file1.txt and file2.txt
 cat file1.txt file2.txt
 
-# Append file1 content into existingfile.txt
-cat file1.txt > existingfile.txt
+# Create existingfile.txt
+echo "Existing file" > existingfile.txt
 
-# Display existing file
+# Append file1.txt to existingfile.txt
+cat file1.txt >> existingfile.txt
+
+# Display existingfile.txt
 cat existingfile.txt
 
-# Numbering Lines
+# Number the lines of newfile.txt
 cat -n newfile.txt
